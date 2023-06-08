d7e3715cf85ffcdf793307cadbe9945c3a04d86e
Andrei Tsarikov <a.tsarikov@ya.ru>
2023-06-08 10:53:52 +0300

    Solved!

--- matrix_multiply.py
+++ matrix_multiply.py
@@ -19,24 +19,28 @@ def possibility(matrix_A, matrix_B):
 
 
 def multiply_matrix(matrix_A: list, matrix_B: list) -> list:
+    matr_A_lines = len(matrix_A)
+    matr_B_lines = len(matrix_B)
+    matr_A_rows = len(matrix_A[0])
+    matr_B_rows = len(matrix_B[0])
+    sum = 0
+    result = [[0 for x in range(matr_B_rows)] for y in range(matr_A_lines)]
+    print
     # if is_matrix(matrix_A) and is_matrix(matrix_B) and possibility(matrix_A, matrix_B):
     if possibility(matrix_A, matrix_B):
-        matr_A_lines = len(matrix_A)
-        matr_B_lines = len(matrix_B)
-        matr_A_rows = len(matrix_A[0])
-        matr_B_rows = len(matrix_B[0])
-        sum = 0
         for i in range(matr_A_rows):
-            for j in range(matr_A_lines):
-                
+            for j in range(matr_B_lines):
+                for k in range(matr_B_rows):
+                    result[i][k] += matrix_A[i][j] * matrix_B[j][k]
 
         # for line in range(matr_A_lines):
         #     for row in range(matr_B_rows):
-    pass 
+    return result 
 
-m1 = [[1,3],[2,8]]
-m2 = [[1,5,9],[3,8,6]]
+m1 = [[1,2],[0,1]]
+m2 = [[2,5,1],[6,7,1]]
 m3 = [1,2]
 
 print(possibility(m1,m2))
 print(is_matrix(m3))
+print(multiply_matrix(m1,m2))
